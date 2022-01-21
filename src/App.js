965382a628a0cfcce0fe8355b76e614666e3c3d9
Ellah Nzikoba <ellahnzikoba@gmail.com>
2022-01-21 03:37:38 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ellah's first React App!
         </p>
         <a
           className="App-link"
