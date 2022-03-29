5b390a0f0ec667e1919a0042b8a6e5acc2c79714
Ellah Nzikoba <ellahnzikoba@gmail.com>
2022-03-28 23:29:33 -0400

    a7 challenge form

--- src/components/tuiter/ProfileEditor/EditProfileBio.js
+++ src/components/tuiter/ProfileEditor/EditProfileBio.js
@@ -1,31 +1,129 @@
-import React from "react";
+import React, {useState} from "react";
+import {useDispatch} from "react-redux";
 
 const ProfileBio = ({user}) => {
+    let [name, setName] = useState(user.first_name + " " + user.last_name);
+    let [location, setLocation] = useState(user.location);
+    let [bio, setBio] = useState(user.biography);
+    let [date, setDate] = useState(user.dob);
+
+    const dispatch = useDispatch();
+
+    const nameChangeHandler = (event) => {
+        const value = event.target.value;
+        setName(value);
+    }
+
+    const nameFocusHandler = (event) => {
+        setName("");
+    }
+
+    const locationChangeHandler = (event) => {
+        const value = event.target.value;
+        setLocation(value);
+    }
+
+    const locationFocusHandler = (event) => {
+        setLocation("");
+    }
+
+    const bioChangeHandler = (event) => {
+        const value = event.target.value;
+        setBio(value);
+    }
+
+    const bioFocusHandler = (event) => {
+        setBio("");
+    }
+
+    const dateChangeHandler = (event) => {
+        const value = event.target.value;
+        setDate(value);
+    }
+
+    const dateFocusHandler = (event) => {
+        setDate("");
+    }
+
+    const nameSubmitHandler = (event) => {
+        event.preventDefault();
+        dispatch({
+            type: "change-name", user: name
+        })
+      setName(name);
+    }
+
+    const locationSubmitHandler = (event) => {
+        event.preventDefault();
+        dispatch({
+            type: "change-location", location: location
+        })
+        setLocation(location);
+    }
+
+    const bioSubmitHandler = (event) => {
+        event.preventDefault();
+        dispatch({
+            type: "change-bio", bio: bio
+        })
+        setBio(bio);
+    }
+
+    const dateSubmitHandler = (event) => {
+        event.preventDefault();
+        dispatch({
+            type: "change-dob", date: date
+        })
+        setDate(date);
+    }
+
     return (
         <div className="d-flex flex-column align-content-between wd-bio">
 
-            <form className="form-floating wd-form mt-5 mb-2">
-                <input type="text" className="form-control" id="name"
-                       placeholder={`${user.first_name} ${user.last_name}`}/>
+            <form className="form-floating wd-form mt-5 mb-2"
+                  onSubmit={nameSubmitHandler}
+            >
+                <input type="text"
+                       className="form-control" id="name"
+                       value={name}
+                       onChange={nameChangeHandler}
+                       onFocus={nameFocusHandler}
+
+                />
                 <label htmlFor="name">Name</label>
             </form>
             <div className="d-flex justify-content-between mb-2">
-                <form className="form-floating wd-form" style={{"width": "45%"}}>
+                <form className="form-floating wd-form" style={{"width": "48%"}}
+                      onSubmit={locationSubmitHandler}
+                >
                     <input type="text" className="form-control" id="location"
-                           placeholder={`${user.location}`}/>
+                           value={location}
+                           onChange={locationChangeHandler}
+                           onFocus={locationFocusHandler}
+                    />
                     <label htmlFor="location">Location</label>
                 </form>
 
-                <form className="form-floating wd-form" style={{"width": "45%"}}>
+                <form className="form-floating wd-form" style={{"width": "48%"}}
+                      onSubmit={dateSubmitHandler}
+                >
                     <input type="date" className="form-control" id="dob"
-                           placeholder={`${user.dob}`}/>
+                           value={date}
+                           onChange={dateChangeHandler}
+                           onFocus={dateFocusHandler}
+                    />
                     <label htmlFor="dob">Name</label>
                 </form>
             </div>
 
-            <form className="form-floating wd-form ">
+            <form className="form-floating wd-form "
+                  onSubmit={bioSubmitHandler}
+            >
                 <textarea className="form-control" id="bio" style={{"height": "80px"}}
-                          placeholder={`${user.biography}`}/>
+                          value={bio}
+                          onChange={bioChangeHandler}
+                          onFocus={bioFocusHandler}
+                />
                 <label htmlFor="bio">Bio</label>
             </form>
 
--- src/components/tuiter/reducers/profile-reducer.js
+++ src/components/tuiter/reducers/profile-reducer.js
@@ -1,7 +1,24 @@
 import user from "../data/user.json";
 
-const profileReducer = (state = user) => {
-    return user;
+const profileReducer = (state = user, action) => {
+    switch(action.type) {
+        case "change-name":
+            user.first_name = action.user.split(" ")[0];
+            user.last_name = action.user.split(" ")[1];
+            return user;
+        case "change-location":
+            user.location = action.location;
+            return user;
+        case "change-dob":
+            user.dob = action.dob;
+            return user;
+        case "change-bio":
+            user.biography = action.bio;
+            return user;
+        default:
+            return user;
+
+    }
 }
 
 export default profileReducer
