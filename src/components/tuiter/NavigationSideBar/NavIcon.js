c6df500c7320e828797afc6d06d98fdb59fe7d2b
Ellah Nzikoba <ellahnzikoba@gmail.com>
2022-03-28 22:34:06 -0400

    a7 routing changes

--- src/components/tuiter/NavigationSideBar/NavIcon.js
+++ src/components/tuiter/NavigationSideBar/NavIcon.js
@@ -2,10 +2,11 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 const NavIcon = (item) => {
-    const page = item.icon.title.toLowerCase();
+    let page = item.icon.title.toLowerCase();
+
 
     return(
-        <Link to={`/tuiter/${page}`}
+        <Link to={`/tuiter/${page === "home" ? "" : page}`}
               className={`list-group-item list-group-item-action ${item.active === item.icon.title ? 'active' : ''}`}>
             <i className={`fa${item.icon.type} fa-${item.icon.icon} pt-0 pb-0 ps-0 pe-1`}/>
             <span className="d-none d-xl-inline"> {item.icon.title}</span>
