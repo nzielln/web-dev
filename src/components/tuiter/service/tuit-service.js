334ca56b4f0907c473745d20841e6de5a8ab78d3
Ellah Nzikoba <ellahnzikoba@gmail.com>
2022-04-06 20:56:01 -0400

    fixed env var 404 error

--- src/components/tuiter/service/tuit-service.js
+++ src/components/tuiter/service/tuit-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 //const TUITS_API = "https://tuiter-server.herokuapp.com/api/tuits";
 
-const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:4000/api';
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuits = async (tuit) => {
